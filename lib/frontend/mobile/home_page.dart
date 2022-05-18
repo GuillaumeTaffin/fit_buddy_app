c6fb81814c85a3419af7cfe1489ca81cce59597e
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-05-18 16:34:57 -0400

    simple home page

--- lib/frontend/mobile/home_page.dart
+++ lib/frontend/mobile/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:fit_buddy_app/frontend/components/helpers.dart';
+import 'package:fit_buddy_app/frontend/components/texts.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,9 +7,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HOME'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 100.0),
+              child: Center(child: Title1(text: 'welcome to fit buddy !')),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                HomePageItem(image: 'images/healthy-lifestyle-border.png', label: 'trainings'),
+                HomePageItem(image: 'images/settings.png', label: 'settings'),
+              ].padItems(const EdgeInsets.all(16)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePageItem extends StatelessWidget {
+  final String image;
+  final String label;
+
+  const HomePageItem({Key? key, required this.image, required this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            image,
+            width: 60,
+          ),
+          Title1(text: label)
+        ].padItems(const EdgeInsets.symmetric(horizontal: 8)),
       ),
     );
   }
